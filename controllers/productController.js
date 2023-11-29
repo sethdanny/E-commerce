9314838f1368244f16f50b68af0e27aac560001a
sethdanny <naddulidaniel1994@gmail.com>
2023-11-29 16:39:14 +0300

    created product routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,3 +11,26 @@ export const createProduct = asyncHandler(
         }    
     }
 );
+
+export const getProduct = asyncHandler(
+    async (req, res) => {
+        const { id } = req.params;
+        try {
+            const findProduct = await Product.findById(id);
+            res.status(200).json(findProduct);
+        } catch (error) {
+            throw new Error(error);     
+        }
+    }
+);
+
+export const getProducts = asyncHandler(
+    async (req, res) => {
+        try {
+            const findProducts = await Product.find();
+            res.status(200).json(findProducts);
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+);
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import {createProduct} from '../controllers/productController.js';
+import {createProduct, getProduct, getProducts} from '../controllers/productController.js';
 
 
 router.post('/create', createProduct);
+router.get('/:id', getProduct);
+router.get('/', getProducts);
 
 
 export default router;
